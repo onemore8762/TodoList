c6ef3d9b175d83c965fd748be29eaafbdaaec91d
Denis <denis.churkin.afikei@mail.ru>
2022-11-24 12:14:40 +0300

    Create Todolist.tsx v. 0.01

---
+++ src/components/Todolist.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type PropsType = {
+    title: string
+    tasks: TaskType[]
+}
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+export const Todolist = (props: PropsType) => {
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li key={props.tasks[0].id}><input type="checkbox" checked={props.tasks[0].isDone}/><span>{props.tasks[0].title}</span></li>
+                <li key={props.tasks[1].id}><input type="checkbox" checked={props.tasks[1].isDone}/><span>{props.tasks[1].title}</span></li>
+                <li key={props.tasks[2].id}><input type="checkbox" checked={props.tasks[2].isDone}/><span>{props.tasks[2].title}</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    )
+}
